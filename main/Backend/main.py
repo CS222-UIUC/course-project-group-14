084be432ee301452e30a0958777390eacb18059d
erikly <erikly2@illinois.edu>
2022-12-02 17:42:03 -0600

    Finds calendar

--- main/Backend/main.py
+++ main/Backend/main.py
@@ -32,6 +32,7 @@ def mergeList(list1, list2, merged_list):
             
 
 def FindingDates():
+    i = 0
     midterm_list = []
     slash_date_list = []
     test_list = ["March 3", "December 24", "Midterm"]
@@ -39,31 +40,43 @@ def FindingDates():
     file1 = open("Main/Backend/parsed.txt", 'r')
     lines = file1.readlines()
     for line in lines:
+        cal = []
+        word = ["calendar"]
+        pattern = r'\W.*?({})\W.*?'.format('|'.join(word))
+        cal = re.findall(pattern, line, flags=re.IGNORECASE)
+        if len(cal) != 0:
+            break
+        i+=1
+    for line in lines:
         #parse
-        word1 = ["midterm"]
-        pattern1 = r'\W.*?({})\W.*?'.format('|'.join(word1))
-        midterm = re.findall(pattern1, line, flags=re.IGNORECASE)
-        if len(midterm) != 0:
-            midterm_list.append(midterm)
+        if i == 0:
+            #word1 = ["midterm"]
+            #pattern1 = r'\W.*?({})\W.*?'.format('|'.join(word1))
+            #midterm = re.findall(pattern1, line, flags=re.IGNORECASE)
+            #if len(midterm) != 0:
+             #   midterm_list.append(midterm)
 
-        word2 = ["test"]
-        pattern2 = r'\W.*?({})\W.*?'.format('|'.join(word2))
-        date_pat = r'((1[0-2])|([1-9]))/(([1-2][0-9])|([1-9])|(3[0-1]))'
-        test = re.findall("^([1-9] |1[0-9]| 2[0-9]|3[0-1])/([1-9] |1[0-2])$", line, flags=re.IGNORECASE)
-        if len(test) != 0:
-            midterm_list.append(test)
-        # begin with 1-31 followed by slash, followed by 1-12
-        # slash_date_list = re.findall("^([1-9] |1[0-9]| 2[0-9]|3[0-1])/([1-9] |1[0-2])$", line, flags=re.IGNORECASE)
-        slash_date_pattern = re.compile('\\b((0[1-9]|1[0-2]|[1-9])/([1-9]|0[1-9]|[1-2][0-9]|3[0-1]))\\b')
-        slash_dates = slash_date_pattern.findall(line)
-        if len(slash_dates) != 0:
-            slash_date_list.append(slash_dates)
-    
+            pattern= [r'\D+']
+            for j in pattern:
+                midterm= re.findall(j, line)
+                midterm_list.append(midterm)
+            
+            # begin with 1-31 followed by slash, followed by 1-12
+            # slash_date_list = re.findall("^([1-9] |1[0-9]| 2[0-9]|3[0-1])/([1-9] |1[0-2])$", line, flags=re.IGNORECASE)
+            slash_date_pattern = re.compile('\\b((0[1-9]|1[0-2]|[1-9])/([1-9]|0[1-9]|[1-2][0-9]|3[0-1]))\\b')
+            slash_dates = slash_date_pattern.findall(line)
+            if len(slash_dates) != 0:
+                slash_date_list.append(slash_dates)
+            continue
+        i-=1
+    #Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private
     findlist = []
     file = open("Main/Backend/result.csv", 'w')
     writer = csv.writer(file)
     findlist.append(midterm_list[0][0])
     findlist.append(slash_date_list[0][0][0])
     writer.writerow(findlist)
+    writer.writerow(midterm_list)
+    writer.writerow(slash_date_list)
             
 
