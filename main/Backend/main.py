dbfadfaa4ef48c12b5d20e63e1c0e3de787d9f8b
erikly <erikly2@illinois.edu>
2022-12-02 19:06:08 -0600

    format of result changed

--- main/Backend/main.py
+++ main/Backend/main.py
@@ -30,6 +30,13 @@ def mergeList(list1, list2, merged_list):
         for x in n:
             merged_list.append(x)
             
+def eventName(list):
+    finalString = ""
+    for i in range(len(list)):
+        if i != 0:
+            finalString+=" "
+        finalString+=list[i]
+    return finalString
 
 def FindingDates():
     i = 0
@@ -50,16 +57,10 @@ def FindingDates():
     for line in lines:
         #parse
         if i == 0:
-            #word1 = ["midterm"]
-            #pattern1 = r'\W.*?({})\W.*?'.format('|'.join(word1))
-            #midterm = re.findall(pattern1, line, flags=re.IGNORECASE)
-            #if len(midterm) != 0:
-             #   midterm_list.append(midterm)
-
-            pattern= [r'\D+']
+            pattern= [r'[a-zA-Z]+']
             for j in pattern:
-                midterm= re.findall(j, line)
-                midterm_list.append(midterm)
+                midterm = re.findall(j, line)
+                midterm_list.append(eventName(midterm))
             
             # begin with 1-31 followed by slash, followed by 1-12
             # slash_date_list = re.findall("^([1-9] |1[0-9]| 2[0-9]|3[0-1])/([1-9] |1[0-2])$", line, flags=re.IGNORECASE)
@@ -70,13 +71,24 @@ def FindingDates():
             continue
         i-=1
     #Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private
-    findlist = []
+    
     file = open("Main/Backend/result.csv", 'w')
     writer = csv.writer(file)
-    findlist.append(midterm_list[0][0])
-    findlist.append(slash_date_list[0][0][0])
-    writer.writerow(findlist)
-    writer.writerow(midterm_list)
-    writer.writerow(slash_date_list)
+    intro = ["Subject","Start Date","Start Time","End Date","End Time","All Day Event","Description","Location","Private"]
+    writer.writerow(intro)
+    for k in range(len(slash_date_list)):
+        findlist = []
+        findlist.append(midterm_list[2+k])
+        findlist.append(slash_date_list[k][0][0]+"/2022")
+        findlist.append("12:00AM")
+        findlist.append(slash_date_list[k][0][0]+"/2022")
+        findlist.append("11:59PM")
+        findlist.append("yes")
+        findlist.append(" ")
+        findlist.append("ONLINE")
+        findlist.append("yes")
+        writer.writerow(findlist)
+    #writer.writerow(midterm_list)
+    #writer.writerow(slash_date_list)
             
 
